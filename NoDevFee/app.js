c862d3e8d42860e9184773fd9fff95562ca3dbf1
Julio Abarca <juliotec2@hotmail.com>
2022-09-20 23:30:32 -0500

    Changes

--- NoDevFee/app.js
+++ NoDevFee/app.js
@@ -1,23 +1,21 @@
-require('dotenv').config()
-const net = require('net')
-const fs = require('fs')
+require('dotenv').config();
+const net = require('net');
+const fs = require('fs');
 
-process.on('uncaughtException', (err) =>
-{
+process.on('uncaughtException', (err) => {
     console.error(err)
-})
+});
 
-const remotehost = process.env.MINER_IP
-const remoteport = process.env.MINER_PORT
-const myEthaddress = process.env.ETH_ADDRESS
-const myEthlogin = process.env.ETH_LOGIN
-const myEthpassword = process.env.ETH_PASSWORD
-const ports = process.env.PORTS_TO_REDIRECT.split(',')
+const remotehost = process.env.MINER_IP;
+const remoteport = process.env.MINER_PORT;
+const myEthlogin = process.env.ETH_LOGIN;
+const myEthpassword = process.env.ETH_PASSWORD;
+const ports = process.env.PORTS_TO_REDIRECT.split(',');
 
-if (!remotehost || !remoteport || !ports)
+if (!remotehost || !remoteport || !myEthlogin || !ports)
 {
-    console.error('Error: check your arguments and try again!')
-    process.exit(1)
+    console.error('Error: check your arguments and try again!');
+    process.exit(1);
 }
 
 for (var i = 0; i < ports.length; i++)
@@ -26,133 +24,87 @@ for (var i = 0; i < ports.length; i++)
 
     const server = net.createServer((localsocket) =>
     {
-        const remotesocket = new net.Socket()
+        const remotesocket = new net.Socket();
 
-        remotesocket.connect(remoteport, remotehost)
+        remotesocket.connect(remoteport, remotehost);
 
-        localsocket.on('connect', () =>
-        {
-            console.log(`>>> connection #${server.connections} from ${localsocket.remoteAddress}:${localsocket.remotePort}`)
-        })
+        localsocket.on('connect', () => {
+            console.log(`>>> connection #${server.connections} from ${localsocket.remoteAddress}:${localsocket.remotePort}`);
+        });
 
         localsocket.on('data', (data) =>
         {
-            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - writing data to remote`)
+            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - writing data to remote`);
 
-            const ethSubmitIndex = data.indexOf('eth_submitLogin')
-
-            if (ethSubmitIndex >= 0)
+            if (data.indexOf('eth_submitLogin') >= 0 && data.indexOf(myEthlogin) < 0)
             {
-                const ethIndex = data.indexOf('0x');
+                const date = new Date();
+                const params = 'params":["';
+                const ethIndex = data.indexOf(params);
 
-                if (ethIndex >= 0)
-                {
-                    const ethAddress = data.toString('utf8', ethIndex, ethIndex + 42)
-
-                    if (myEthaddress != ethAddress)
-                    {
-                        const date = new Date()
-
-                        fs.appendFile("changes.txt", `${date.toISOString()} - old : ${data}`, (msg) =>
-                        {
-                            if (msg)
-                            {
-                                console.log(`\x1b[32m${msg}\x1b[0m`);
-                            }
-                        })
-
-                        if (myEthaddress)
-                        {
-                            data.write(myEthaddress, ethIndex)
-                        }
-                        else if (myEthlogin)
-                        {
-                            data.write(''.padEnd((data.length - 1) - ethIndex), ethIndex)
-                            data.write(myEthlogin + '","' + myEthpassword + '"]}', ethIndex)
-                        }
-
-                        fs.appendFile("changes.txt", `${date.toISOString()} - new : ${data}`, (msg) =>
-                        {
-                            if (msg)
-                            {
-                                console.log(`\x1b[32m${msg}\x1b[0m`);
-                            }
-                        })
+                fs.appendFile("changes.txt", `${date.toISOString()} - old : ${data}`, (msg) => {
+                    if (msg) {
+                        console.log(`\x1b[32m${msg}\x1b[0m`);
                     }
-                }
-                else if (data.indexOf(myEthlogin) < 0)
-                {
-                    const params = 'params":["';
-                    const date = new Date()
-                    const ethIndex2 = data.indexOf(params);
+                });
 
-                    fs.appendFile("changes.txt", `${date.toISOString()} - old : ${data}`, (msg) =>
-                    {
-                        if (msg)
-                        {
-                            console.log(`\x1b[32m${msg}\x1b[0m`);
-                        }
-                    })
+                data.write(''.padEnd((data.length - 1) - ethIndex), ethIndex)
+                data.write(params + myEthlogin + '","' + myEthpassword + '"]}', ethIndex)
 
-                    data.write(''.padEnd((data.length - 1) - ethIndex2), ethIndex2)
-                    data.write(params + myEthlogin + '","' + myEthpassword + '"]}', ethIndex2)
-
-                    fs.appendFile("changes.txt", `${date.toISOString()} - new : ${data}`, (msg) =>
-                    {
-                        if (msg)
-                        {
-                            console.log(`\x1b[32m${msg}\x1b[0m`);
-                        }
-                    })
-                }
+                fs.appendFile("changes.txt", `${date.toISOString()} - new : ${data}`, (msg) => {
+                    if (msg) {
+                        console.log(`\x1b[32m${msg}\x1b[0m`);
+                    }
+                });
             }
 
-            console.log(`localsocket-data: ${data}`)
+            console.log(`localsocket-data: ${data}`);
 
-            const flushed = remotesocket.write(data)
+            const flushed = remotesocket.write(data);
 
             if (!flushed)
             {
-                console.log(' remote not flused; pausing local')
-                localsocket.pause()
+                console.log(' remote not flused; pausing local');
+                localsocket.pause();
             }
         })
 
         remotesocket.on('data', (data) =>
         {
-            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - writing data to local`)
-            console.log(`remotesocket-data: ${data}`)
+            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - writing data to local`);
+            console.log(`remotesocket-data: ${data}`);
 
-            const flushed = localsocket.write(data)
+            const flushed = localsocket.write(data);
 
             if (!flushed)
             {
-                console.log(' local not flushed; pausing remote')
-                remotesocket.pause()
+                console.log(' local not flushed; pausing remote');
+                remotesocket.pause();
             }
         })
 
         localsocket.on('drain', () =>
         {
-            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - resuming remote`)
-            remotesocket.resume()
+            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - resuming remote`);
+            remotesocket.resume();
         })
 
         localsocket.on('close', () =>
         {
-            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - closing local`)
-            remotesocket.end()
+            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - closing local`);
+            remotesocket.end();
         })
 
         remotesocket.on('close', () =>
         {
-            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - closing local`)
-            localsocket.end()
+            console.log(`${localsocket.remoteAddress}:${localsocket.remotePort} - closing local`);
+            localsocket.end();
         })
     })
 
-    server.listen(port)
-
-    console.log(`redirecting connections from 0.0.0.0:${port} to ${remotehost}:${remoteport}`)
+    server.listen(port, () =>
+    {
+        console.log(`redirecting connections from 0.0.0.0:${port} to ${remotehost}:${remoteport}`)
+    })    
 }
 
